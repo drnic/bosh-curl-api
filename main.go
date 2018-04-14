08704512110a7e17f443be26c831eb15b7eeacd3
Dr Nic Williams <drnicwilliams@gmail.com>
2018-04-15 07:36:50 +1000

    refactor into boshcli.Check()

--- main.go
+++ main.go
@@ -1,35 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"os/exec"
+	"github.com/drnic/bosh-curl-example/boshcli"
 )
 
 func main() {
-	// check that 'bosh' available
-	cmd := exec.Command("sh", "-c", "bosh -h")
-	stdoutStderr, err := cmd.CombinedOutput()
-	if err != nil {
-		fmt.Printf("%s\n", stdoutStderr)
-		log.Fatal("Install 'bosh' from https://github.com/cloudfoundry/bosh-cli/pull/408")
-	}
-
-	// check that 'bosh curl' available
-	cmd = exec.Command("sh", "-c", "bosh curl -h")
-	stdoutStderr, err = cmd.CombinedOutput()
-	if err != nil {
-		fmt.Printf("%s\n", stdoutStderr)
-		log.Fatal("Need 'bosh curl' from https://github.com/cloudfoundry/bosh-cli/pull/408")
-	}
-
-	// check that bosh environment configured and connectable
-	cmd = exec.Command("sh", "-c", "bosh curl /info")
-	stdoutStderr, err = cmd.CombinedOutput()
-	if err != nil {
-		fmt.Printf("%s\n", stdoutStderr)
-		log.Fatal("Cannot connect to BOSH")
-	}
-
-	fmt.Printf("%s\n", stdoutStderr)
+	boshcli.Check()
 }
